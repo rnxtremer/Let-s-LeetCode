c02c018b12ed7280b1cd55ecd81dc709509714df
rn_Xtremer_ <119094662+rnxtremer@users.noreply.github.com>
2024-05-01 19:54:45 +0530

    Time: 1 ms (49.93%) | Memory: 41.4 MB (81.91%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch); 
+        if (index == -1) {
+            return word; 
+        }
+
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        sb.reverse(); 
+        return sb.toString() + word.substring(index + 1); 
+    }
+}
